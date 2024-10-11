6b805705bfaedd0f4d9c7c856a2cc6b644a3e706
Maximilian <scientificresearch309@gmail.com>
2024-10-11 22:49:28 +0200

    use strict mode shows us the possible bugs or errors in our code

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict"; /* we add this Expression to display us the possible errors or Bugs in our code!*/
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
