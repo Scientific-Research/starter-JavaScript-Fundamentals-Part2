6e1cb56ba4de5e10f72372e87cea4a1ef8b7c6de
Maximilian <scientificresearch309@gmail.com>
2024-10-12 15:35:13 +0200

    and now the coding challenge works for the JS fundamentals-2

--- Coding-Challenge-1.js
+++ Coding-Challenge-1.js
@@ -1,19 +1,33 @@
-/* 1.first part */
-const calcAverage = (a, b, c, n) => {
-  const scoreAverage = ((a, b, c) / 3).toFixed(2);
-  // return scoreAverage;
-  return `${scoreAverage} for the ${n} time!`;
+/* Part 1 */
+const calcAverage = (a, b, c) => {
+  const scoreAverage = ((a + b + c) / 3).toFixed(2);
+  return `${scoreAverage}`;
 };
 
-const scoreAverage = calcAverage(44, 23, 71, 1);
+const scoreAverage = calcAverage(44, 23, 71);
 console.log(scoreAverage);
 
-/* doing the same solution but with normal function instead of arrow function: */
-function calcAverage2(a, b, c, n) {
-  const scoreAverage = ((a, b, c) / 3).toFixed(2);
-  // return scoreAverage;
-  return `${scoreAverage} for the ${n} time!`;
-}
+/* 3. */
+const checkWinner = (avgDolphins, avgKoalas) => {
+  if (avgDolphins >= 2 * avgKoalas) {
+    return `Dolphins team wins (${avgDolphins} vs. ${avgKoalas})`;
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    return `Koalas team wins (${avgKoalas} vs. ${avgDolphins})`;
+  } else {
+    return `No team wins!`;
+  }
+};
 
-const scoreAverage_new = calcAverage2(44, 23, 71, 1);
-console.log(scoreAverage_new);
+/* Part 2 */
+const calcAverageForBothTeams = (a, b, c, d, e, f) => {
+  const avgDolphins = ((a + b + c) / 3).toFixed(2);
+  const avgKoalas = ((d + e + f) / 3).toFixed(2);
+  console.log(avgDolphins, avgKoalas);
+
+  const checkWinnerResult = checkWinner(avgDolphins, avgKoalas);
+  // return `${avgDolphins} ${avgKoalas}`;
+  return checkWinnerResult;
+};
+
+const scoreAverage2 = calcAverageForBothTeams(85, 54, 41, 23, 34, 27);
+console.log(scoreAverage2);
