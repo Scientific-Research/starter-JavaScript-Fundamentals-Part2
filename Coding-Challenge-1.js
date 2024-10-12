7958b1844be544fc5611bee78dff89ccb7c282f2
Maximilian <scientificresearch309@gmail.com>
2024-10-12 14:44:55 +0200

    till now, doing the slution with normal function and arrow function

--- Coding-Challenge-1.js
+++ Coding-Challenge-1.js
@@ -1,1 +1,19 @@
+/* 1.first part */
+const calcAverage = (a, b, c, n) => {
+  const scoreAverage = ((a, b, c) / 3).toFixed(2);
+  // return scoreAverage;
+  return `${scoreAverage} for the ${n} time!`;
+};
 
+const scoreAverage = calcAverage(44, 23, 71, 1);
+console.log(scoreAverage);
+
+/* doing the same solution but with normal function instead of arrow function: */
+function calcAverage2(a, b, c, n) {
+  const scoreAverage = ((a, b, c) / 3).toFixed(2);
+  // return scoreAverage;
+  return `${scoreAverage} for the ${n} time!`;
+}
+
+const scoreAverage_new = calcAverage2(44, 23, 71, 1);
+console.log(scoreAverage_new);
