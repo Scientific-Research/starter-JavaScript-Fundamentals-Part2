ae1d303cf226f4544660411d535e364af202bfb5
Maximilian <scientificresearch309@gmail.com>
2024-10-14 16:22:21 +0200

    some more explanations for the code challenging

--- Coding-Challenge-3.js
+++ Coding-Challenge-3.js
@@ -6,8 +6,10 @@ const mark = {
   height: 1.69,
 
   calcBMI: function () {
-    const bmi = `${this.mass}` / `${this.height ** 2}`;
-    return bmi.toFixed(2);
+    this.bmi = (`${this.mass}` / `${this.height ** 2}`).toFixed(2);
+    /* this.bmi creates bmi property in mark object and we can access that like other properties in this object using "this" keyword!*/
+    return this.bmi;
+    /* we return it to call it directly without using calcBMI function, because it is already a property of this object like other properties like firsName, lastName,... */
   },
 };
 
@@ -18,8 +20,10 @@ const john = {
   height: 1.95,
 
   calcBMI: function () {
-    const bmi = `${this.mass}` / `${this.height ** 2}`;
-    return bmi.toFixed(2);
+    this.bmi = (`${this.mass}` / `${this.height ** 2}`).toFixed(2);
+    /* this.bmi creates bmi property in mark object and we can access that like other properties in this object using "this" keyword!*/
+    return this.bmi;
+    /* we return it to call it directly without using calcBMI function, because it is already a property of this object like other properties like firsName, lastName,... */
   },
 };
 
@@ -27,6 +31,10 @@ const john = {
 /* the method adde above in each object separately! */
 console.log("Mark's BMI: " + mark.calcBMI());
 console.log("John's BMI: " + john.calcBMI());
+
+console.log(john.bmi);
+/* we can call the bmi property directly without calling its function(calcBMI()), because we created it in calcBMI() function above using the "this" keyword which means the object(mark/john) itself! */
+console.log(mark.bmi);
 
 /* 3. */
 /* using Ternary operator: */
