9b4451b9aa9788506fd423f6310d41e200db2c0c
Maximilian <scientificresearch309@gmail.com>
2024-10-14 15:19:37 +0200

    first solution for code challenging using the ternary operator

--- Coding-Challenge-3.js
+++ Coding-Challenge-3.js
@@ -2,16 +2,42 @@
 const mark = {
   firstName: "Mark",
   lastName: "Miller",
-  weight: 78,
+  mass: 78,
   height: 1.69,
+
+  calcBMI: function () {
+    const bmi = `${this.mass}` / `${this.height ** 2}`;
+    return bmi.toFixed(2);
+  },
 };
 
 const john = {
   firstName: "John",
   lastName: "Smith",
-  weight: 92,
+  mass: 92,
   height: 1.95,
+
+  calcBMI: function () {
+    const bmi = `${this.mass}` / `${this.height ** 2}`;
+    return bmi.toFixed(2);
+  },
 };
 
 /* 2. */
+/* the method adde above in each object separately! */
+console.log("Mark's BMI: " + mark.calcBMI());
+console.log("John's BMI: " + john.calcBMI());
 
+/* 3. */
+/* using Ternary operator: */
+console.log(
+  `${mark.calcBMI()}` > `${john.calcBMI()}`
+    ? `${mark.firstName}'s BMI (${mark.calcBMI()}) is higher than ${
+        john.firstName
+      }'s (${john.calcBMI()})!`
+    : `${john.firstName}'s BMI (${john.calcBMI()}) is higher than ${
+        mark.firstName
+      }'s (${mark.calcBMI()})!`
+);
+
+/* using if-else: */
