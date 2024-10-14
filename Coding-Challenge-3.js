50fe6b89d879a80c013bfbe9415e8a40d4884df6
Maximilian <scientificresearch309@gmail.com>
2024-10-14 14:29:43 +0200

    first part of the coding challenge Nr.3 finished

---
+++ Coding-Challenge-3.js
@@ -0,0 +1,17 @@
+/* 1. */
+const mark = {
+  firstName: "Mark",
+  lastName: "Miller",
+  weight: 78,
+  height: 1.69,
+};
+
+const john = {
+  firstName: "John",
+  lastName: "Smith",
+  weight: 92,
+  height: 1.95,
+};
+
+/* 2. */
+
